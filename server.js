738c74b85d4c790997b07777767c8cc765a141ab
Bryant Trinh <trinhbryant@gmail.com>
2022-12-19 13:57:07 -0800

    updated connection.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const { init } = require('./models/Product');
 const routes = require('./routes');
 // import sequelize connection
+
+// const Sequelize - require('sequelize')
+const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +15,25 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Application is listening at port ${PORT}`);
+  });
 });
+
+// // creating new sequelize instance, passing in database connection details as the arguments
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: 'mysql2'
+// });
+
+// // authenticate method
+// sequelize
+// .authenticate()
+// .then(() => {
+//   console.log('Connection has been established succesfully');
+// })
+// .catch(err => {
+//   console.error('Unable to connect to the database:', err);
+// });
