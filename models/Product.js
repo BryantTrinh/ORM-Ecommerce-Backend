ff4565b0f38272cb5a32be75c99e1589e7ae4c9e
Bryant Trinh <trinhbryant@gmail.com>
2022-12-19 19:08:10 -0800

    product.js model done

--- models/Product.js
+++ models/Product.js
@@ -7,9 +7,40 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
+// We need id, product_name, price, stock, category_id,sequelize at the end, no timestamps, freeze table true, model:name is product.
 Product.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+    product_name: {
+      type: DataTypes.STRING
+      allowNull: false,
+    },
+    // DECIMAL (12,2) so we can store 10 digits and 2 digits after the decimal. We need to add a validator to see if it is a decimal
+    price: {
+      type: DataTypes.DECIMAL(12,2),
+      allowNull: false,
+      validate { isDecimal: true,},
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: { isNumeric: true,},
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      // references to category model and its key id
+      references: { 'category',
+      key: 'id',
+      },
+    },
   },
   {
     sequelize,
